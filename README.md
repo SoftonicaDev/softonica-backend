57901bd15abb3d2f731bb93b090a8fc5618c2b22
unknown <domKul>
2024-11-11 19:43:46 +0100

    initial commit witm java-spring

---
+++ README.md
@@ -0,0 +1,1 @@
+# Softonica Frontend
---
+++ src/test/java/com/softonicabackend/TestcontainersConfiguration.java
@@ -0,0 +1,8 @@
+package com.softonicabackend;
+
+import org.springframework.boot.test.context.TestConfiguration;
+
+@TestConfiguration(proxyBeanMethods = false)
+class TestcontainersConfiguration {
+
+}
